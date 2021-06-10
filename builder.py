a43648d9f771f4ea6193f003a64b4c785cd8070e
antonioag95 <30556866+antonioag95@users.noreply.github.com>
2021-06-10 20:19:45 +0200

    Update builder.py

--- builder.py
+++ builder.py
@@ -1,66 +1,184 @@
-from rdflib import Graph, Literal, RDF, RDFS, URIRef
-# rdflib knows about some namespaces, like FOAF
-from rdflib.namespace import XSD, OWL
+from rdflib import Graph, Literal, RDF, URIRef
+from rdflib.namespace import RDF, RDFS, OWL, XSD
 from rdflib import Namespace
 
-bene = "Ultima_cena"
-
-arcoCd = Namespace("https://w3id.org/arco/ontology/context-description/")
+arcoSD = Namespace("https://domain/smartology/semiotic-description/")
 arco = Namespace("https://w3id.org/arco/ontology/arco/")
 
-def connotativeDescription():
-	# create a Graph
-	g = Graph()
-	g.bind("arco", arco)
-	g.bind("a-cd", arcoCd)
+#Create a graph
+g = Graph()
+g.bind("arco", arco)
+g.bind("s-sd", arcoSD)
 
-	# Create an RDF URI node to use as the subject for multiple triples
-	subject = URIRef("{}/TheLastSupper/".format(arco.name))
+def semioticDescription():
+	# Semiotic Description
+	g.add((arcoSD.hasWikiDescription, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasWikiDescription, RDFS.domain, arco.CulturalProperty))
+	g.add((arcoSD.hasWikiDescription, RDFS.range, RDFS.Literal))
 
-	# Add triples using store's add() method.
-	g.add((subject, RDF.type, arco.CulturalProperty))
-	g.add((arcoCd.Interpretation, RDFS.subClassOf, arcoCd.ConnotativeDescription))
-	g.add((arcoCd.Message, RDFS.subClassOf, arcoCd.Interpretation))
-	g.add((arcoCd.Topic, RDFS.subClassOf, arcoCd.Interpretation))
-	g.add((arcoCd.hasMessage, RDF.type, OWL.ObjectProperty))
-	g.add((arcoCd.Message, arcoCd.hasMessage, Literal("Qualcosa", datatype=XSD.string)))
-	g.add((arcoCd.hasSource, RDF.type, OWL.ObjectProperty))
-	g.add((arcoCd.Message, arcoCd.hasSource, Literal("Wikipedia?", datatype=XSD.string)))
-	g.add((arcoCd.hasTopic, RDF.type, OWL.ObjectProperty))
-	g.add((arcoCd.Topic, arcoCd.hasTopic, Literal("Topic", datatype=XSD.string)))
+	g.add((arco.CulturalProperty, RDF.type, OWL.Class))
+
+	g.add((arcoSD.hasSemioticDescription, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasSemioticDescription, RDFS.domain, arco.CulturalProperty))
+	g.add((arcoSD.hasSemioticDescription, RDFS.range, arcoSD.SemioticDescription))
+
+	g.add((arcoSD.SemioticDescription, RDF.type, OWL.Class))
+	g.add((arcoSD.SemioticDescription, RDFS.subClassOf, arco.CulturalProperty))
+
+	g.add((arcoSD.hasExpressiveDescription, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasExpressiveDescription, RDFS.domain, arcoSD.SemioticDescription))
+	g.add((arcoSD.hasExpressiveDescription, RDFS.range, arcoSD.ExpressiveDescription))
+
+	g.add((arcoSD.ExpressiveDescription, RDF.type, OWL.Class))
+	g.add((arcoSD.ExpressiveDescription, RDFS.subClassOf, arcoSD.SemioticDescription))
+
+	g.add((arcoSD.hasCulturalMovement, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasCulturalMovement, RDFS.domain, arcoSD.SemioticDescription))
+	g.add((arcoSD.hasCulturalMovement, RDFS.range, arcoSD.CulturalMovement))
+	
+	g.add((arcoSD.CulturalMovement, RDF.type, OWL.Class))
+	g.add((arcoSD.CulturalMovement, RDFS.subClassOf, arcoSD.SemioticDescription))
+	
+
+	g.add((arcoSD.hasConnotativeDescription, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasConnotativeDescription, RDFS.domain, arcoSD.SemioticDescription))
+	g.add((arcoSD.hasConnotativeDescription, RDFS.range, arcoSD.ConnotativeDescription))
+	
+	g.add((arcoSD.ConnotativeDescription, RDF.type, OWL.Class))
+	g.add((arcoSD.ConnotativeDescription, RDFS.subClassOf, arcoSD.SemioticDescription))
+
+	
+	g.add((arcoSD.hasDenotativeDescription, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasDenotativeDescription, RDFS.domain, arcoSD.SemioticDescription))
+	g.add((arcoSD.hasDenotativeDescription, RDFS.range, arcoSD.DenotativeDescription))
+	
+	g.add((arcoSD.DenotativeDescription, RDF.type, OWL.Class))
+	g.add((arcoSD.DenotativeDescription, RDFS.subClassOf, arcoSD.SemioticDescription))
 
 	ont = g.serialize(format='turtle').decode("utf-8")
-	print(ont)
+	return ont
+
+def connotativeDescription():
+	# Connotative Description
+	g.add((arcoSD.ConnotativeDescription, RDF.type, OWL.Class))
+	g.add((arcoSD.Interpretation, RDF.type, OWL.Class))
+	g.add((arcoSD.Interpretation, RDFS.subClassOf, arcoSD.ConnotativeDescription))
+	g.add((arcoSD.hasInterpretation, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasInterpretation, RDFS.domain, arcoSD.ConnotativeDescription))
+	g.add((arcoSD.hasInterpretation, RDFS.range, arcoSD.Interpretation))
+	g.add((arcoSD.hasMessageInterpretation, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasMessageInterpretation, RDFS.domain, arcoSD.Interpretation))
+	g.add((arcoSD.hasMessageInterpretation, RDFS.range, arcoSD.Message))
+	g.add((arcoSD.Message, RDFS.subClassOf, arcoSD.Interpretation))
+	g.add((arcoSD.hasMessage, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasMessage, RDFS.domain, arcoSD.Message))
+	g.add((arcoSD.hasMessage, RDFS.range, RDFS.Literal))
+	g.add((arcoSD.hasSource, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasSource, RDFS.domain, arcoSD.Message))
+	g.add((arcoSD.hasSource, RDFS.range, RDFS.Literal))
+	g.add((arcoSD.hasTopic, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasTopic, RDFS.domain, arcoSD.Interpretation))
+	g.add((arcoSD.hasTopic, RDFS.range, RDFS.Literal))
+
+	ont = g.serialize(format='turtle').decode("utf-8")
 	return ont
 
 def expressiveDescription():
-	# create a Graph
-	g = Graph()
-	g.bind("arco", arco)
-	g.bind("a-cd", arcoCd)
+	# Expressive Detection
+	g.add((arcoSD.ExpressiveDescription, RDF.type, OWL.Class))
+	g.add((arcoSD.hasColour, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasColour, RDFS.domain, arcoSD.ExpressiveDescription))
+	g.add((arcoSD.hasColour, RDFS.range, RDFS.Literal))
+	g.add((arcoSD.hasLights, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasLights, RDFS.domain, arcoSD.ExpressiveDescription))
+	g.add((arcoSD.hasLights, RDFS.range, RDFS.Literal))
+	ont = g.serialize(format='turtle').decode("utf-8")
 
-	# Create an RDF URI node to use as the subject for multiple triples
-	subject = URIRef("{}/TheLastSupper/".format(arco.name))
+	return ont
 
-	g.add((subject, RDF.type, arco.CulturalProperty))
-	g.add((arcoCd.Colour, RDFS.subClassOf, arcoCd.ExpressiveDescription))
-	g.add((arcoCd.Lights, RDFS.subClassOf, arcoCd.ExpressiveDescription))
-	g.add((arcoCd.hasColour, RDF.type, OWL.ObjectProperty))
-	g.add((arcoCd.Colour, arcoCd.hasColour, Literal("Colore", datatype=XSD.string)))
-	g.add((arcoCd.hasLights, RDF.type, OWL.ObjectProperty))
-	g.add((arcoCd.Lights, arcoCd.hasLights, Literal("Luci", datatype=XSD.string)))
+def denotativeDescription():
+	# Denotative description
+	g.add((arcoSD.DenotativeDescription, RDF.type, OWL.Class))
+	g.add((arcoSD.hasDenotativeTypeDefinition, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasDenotativeTypeDefinition, RDFS.domain, arcoSD.DenotativeDescription))
+	g.add((arcoSD.hasDenotativeTypeDefinition, RDFS.range, RDFS.Literal))
+
+	g.add((arcoSD.hasScene, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasScene, RDFS.domain, arcoSD.DenotativeDescription))
+	g.add((arcoSD.hasScene, RDFS.range, arcoSD.Scene))
+
+	g.add((arcoSD.Scene, RDF.type, OWL.Class))
+	g.add((arcoSD.Scene, RDFS.subClassOf, arcoSD.DenotativeDescription))
+	
+	g.add((arcoSD.hasDescriptionScene, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasDescriptionScene, RDFS.domain, arcoSD.Scene))
+	g.add((arcoSD.hasDescriptionScene, RDFS.range, RDFS.Literal))
+	g.add((arcoSD.hasEnvironment, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasEnvironment, RDFS.domain, arcoSD.Scene))
+	g.add((arcoSD.hasEnvironment, RDFS.range, RDFS.Literal))
+
+	g.add((arcoSD.hasComposition, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasComposition, RDFS.domain, arcoSD.DenotativeDescription))
+	g.add((arcoSD.hasComposition, RDFS.range, arcoSD.Composition))
+
+	g.add((arcoSD.Composition, RDF.type, OWL.Class))
+	g.add((arcoSD.Composition, RDFS.subClassOf, arcoSD.DenotativeDescription))
+
+	g.add((arcoSD.hasSubject, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasSubject, RDFS.domain, arcoSD.Composition))
+	g.add((arcoSD.hasSubject, RDFS.range, RDFS.Literal))
+
+	g.add((arcoSD.hasElementsComposition, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasElementsComposition, RDFS.domain, arcoSD.Composition))
+	g.add((arcoSD.hasElementsComposition, RDFS.range, arcoSD.ElementsComposition))
+
+	g.add((arcoSD.ElementsComposition, RDF.type, OWL.Class))
+	g.add((arcoSD.ElementsComposition, RDFS.subClassOf, arcoSD.Composition))
+
+	g.add((arcoSD.hasForegroundElements, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasForegroundElements, RDFS.domain, arcoSD.ElementsComposition))
+	g.add((arcoSD.hasForegroundElements, RDFS.range, RDFS.Literal))
+
+	g.add((arcoSD.hasBackgroundElements, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasBackgroundElements, RDFS.domain, arcoSD.ElementsComposition))
+	g.add((arcoSD.hasBackgroundElements, RDFS.range, RDFS.Literal))
+
+	g.add((arcoSD.hasRelationsComposition, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasRelationsComposition, RDFS.domain, arcoSD.Composition))
+	g.add((arcoSD.hasRelationsComposition, RDFS.range, arcoSD.RelationsComposition))
+
+	g.add((arcoSD.RelationsComposition, RDF.type, OWL.Class))
+	g.add((arcoSD.RelationsComposition, RDFS.subClassOf, arcoSD.Composition))
+
+	g.add((arcoSD.hasPerspectiveStudy, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasPerspectiveStudy, RDFS.domain, arcoSD.RelationsComposition))
+	g.add((arcoSD.hasPerspectiveStudy, RDFS.range, RDFS.Literal))
+
+	g.add((arcoSD.hasElementRelationships, RDF.type, OWL.ObjectProperty))
+	g.add((arcoSD.hasElementRelationships, RDFS.domain, arcoSD.RelationsComposition))
+	g.add((arcoSD.hasElementRelationships, RDFS.range, arcoSD.ElementRelationships))
+
+	g.add((arcoSD.ElementRelationships, RDF.type, OWL.Class))
+	g.add((arcoSD.ElementRelationships, RDFS.subClassOf, arcoSD.RelationsComposition))
+
+	g.add((arcoSD.hasElementRelationshipsType, RDF.type, OWL.DatatypeProperty))
+	g.add((arcoSD.hasElementRelationshipsType, RDFS.domain, arcoSD.ElementRelationships))
+	g.add((arcoSD.hasElementRelationshipsType, RDFS.range, RDFS.Literal))
 
 	ont = g.serialize(format='turtle').decode("utf-8")
-	print(ont)
 	return ont
+
 
 def saveToFile(ontology):
 	with open("ontology.ttl", "w") as ontFile:
 		ontFile.write(ontology)
 
 def main():
-	ontology = expressiveDescription()
-	saveToFile(ontology)
+	semioticDescription()
+	connotativeDescription()
+	expressiveDescription()
+	complete = denotativeDescription()
+	saveToFile(complete)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	main()
